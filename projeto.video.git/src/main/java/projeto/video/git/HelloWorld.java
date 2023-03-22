ea7e954ea386117fab64c741ed9eb6b78a073bec
nathalia_deodato <nathaliadeodatoc@gmail.com>
2023-03-21 21:13:15 -0300

    inclusão da mensagem git hub

--- projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
+++ projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
@@ -3,8 +3,9 @@ package projeto.video.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("Hello World");
+System.out.println("Git Hub");
 	}
 
 }
